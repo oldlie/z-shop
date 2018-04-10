482a91204f389c8b75d37652ea1fbb66d4365ad0
oldlie <chsq00@163.com>
2018-04-10 20:13:14 +0800

    initialize project

---
+++ src/main/java/com/iprzd/zshop/ZShopApplication.java
@@ -0,0 +1,12 @@
+package com.iprzd.zshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZShopApplication.class, args);
+    }
+}
