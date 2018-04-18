1333fb665b8cb7e2d03853d3f9a956619514b7d9
oldlie <chsq00@163.com>
2018-04-18 09:00:40 +0800

    navigation update

--- admin-ui/src/app/navigation/add/add.component.ts
+++ admin-ui/src/app/navigation/add/add.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { NavigationVi, NavigationType } from '../navigation-vi';
+import { ElNotificationService } from 'element-angular';
 
 @Component({
   selector: 'app-navigation-add',
@@ -13,7 +14,7 @@ export class AddComponent implements OnInit {
   viewType: NavigationType;
   showListDialog = false;
 
-  constructor() { }
+  constructor(private notify: ElNotificationService) { }
 
   ngOnInit() {
     this.model = {
@@ -25,9 +26,19 @@ export class AddComponent implements OnInit {
 
   recevieParent(item: NavigationVi) {
     console.log('Receive Parent Nav:', item);
+    this.model.parentId = item.id;
+    this.showListDialog = false;
+    console.log(this.model);
   }
 
   asignParent() {
     this.showListDialog = true;
+  }
+
+  save(event: Event) {
+    if (this.model.title === '') {
+      this.notify.warning('请填写导航名称');
+      return;
+    }
   }
 }
