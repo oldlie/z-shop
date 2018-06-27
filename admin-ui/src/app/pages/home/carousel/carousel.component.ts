c874eb8a73a3cc7b6a7a446520511005fdf8bf3e
chenlie <434930805@qq.com>
2018-06-27 19:47:13 +0800

    设置轮播图

--- admin-ui/src/app/pages/home/carousel/carousel.component.ts
+++ admin-ui/src/app/pages/home/carousel/carousel.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FileResponse, UploadFile as File } from '../../../response/response';
+import { UploadFile } from 'ng-zorro-antd';
 
 @Component({
   selector: 'app-carousel',
@@ -8,10 +10,44 @@ import { Component, OnInit } from '@angular/core';
 export class CarouselComponent implements OnInit {
 
   list = [1, 2, 3, 4];
+  title = '';
+  titleTip = '';
+  url = '';
+  urlTip = '';
+  sequence = 0;
+
+  uploadUrl = '';
+  fileList = [
+    {
+      uid: -1,
+      name: 'xxx.png',
+      status: 'done',
+      url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png'
+    }
+  ];
+  previewImage = '';
+  previewVisible = false;
+  uploadFileList = [];
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  uploadChange(change: any) {
+    if (change['type'] === 'success') {
+      const response = change['file']['response'] as FileResponse;
+      console.log(response);
+      this.uploadFileList = response.list;
+    }
+  }
+
+  handlePreview = (file: UploadFile) => {
+    this.previewImage = file.url || file.thumbUrl;
+    this.previewVisible = true;
+  }
+
+  save() {
+
+  }
 }
