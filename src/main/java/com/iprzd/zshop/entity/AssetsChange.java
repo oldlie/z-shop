565b48e7d9221a19507dca55f410078d2995b815
chenlie <434930805@qq.com>
2018-11-24 11:11:55 +0800

    不知道什么东西

--- src/main/java/com/iprzd/zshop/entity/AssetsChange.java
+++ src/main/java/com/iprzd/zshop/entity/AssetsChange.java
@@ -4,11 +4,17 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
 import java.io.Serializable;
 import java.util.Date;
 
 @Entity
 @Table(name = "t_assets_change")
+@Setter
+@Getter
 public class AssetsChange implements Serializable {
 
     private final static long serialVersionUID = 1540128117909L;
